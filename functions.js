eaff5bdb5a756d3b4bff311785a140cd53db37b2
Mike Oram <mike.oram@mayden.co.uk>
2022-10-05 15:50:49 +0100

    create v1 of testable functions

---
+++ functions.js
@@ -0,0 +1,66 @@
+
+/*
+ * Must provide DOB in YYYY-MM-DD format
+ */
+function ageCalculator(dob) {
+    const dobDate = new Date(dob)
+    if (dobDate.toString() === 'Invalid Date') {
+        return false
+    }
+    const today = new Date()
+    let age = today.getFullYear() - dobDate.getFullYear()
+    let months = today.getMonth() - dobDate.getMonth()
+    if (months < 0 || (months === 0 && today.getDate() < dobDate.getDate())) {
+        age--;
+    }
+    if (age < 0) return false
+    return age;
+}
+
+function taxCalculator(salary) {
+    const taxBrackets = [
+        {
+            amount: 12500,
+            percentage: 0
+        },
+        {
+            amount: 17500,
+            percentage: 0.05
+        },
+        {
+            amount: 20000,
+            percentage: 0.1
+        },
+        {
+            percentage: 0.2
+        }
+    ]
+    let bracket = 0
+    let tax = 0
+    while (salary > 0) {
+        if (bracket > 2 || salary <= taxBrackets[bracket].amount) {
+            tax += salary * taxBrackets[bracket].percentage
+        } else {
+            tax += taxBrackets[bracket].amount * taxBrackets[bracket].percentage
+        }
+        salary -= taxBrackets[bracket].amount
+        bracket++
+    }
+    return tax
+}
+
+function greetBot(name, showWeather = true) {
+    let weather = ''
+    if (showWeather) {
+        const weatherOptions = [
+            'It is sunny today!',
+            'It is a bit chilly today.',
+            'It is raining today.',
+            'It is snowing.'
+        ]
+
+        weather = ' ' + weatherOptions[Math.floor(Math.random() * weatherOptions.length)]
+    }
+
+    return 'Hi ' + name + '.' + weather
+}
