a4ab4a3e1dfcc89f7bef417432b743be4d978b63
Adam Young <adamyoungy678@gmail.com>
2023-06-08 11:32:40 +0100

    feat: added test for calculating tax

--- __test__/functions.test.js
+++ __test__/functions.test.js
@@ -1,5 +1,7 @@
 const functions = require('../functions')
 const { calculateAge } = require('../functions');
+const { calculateTax } = require('../functions');
+
 describe('calculateAge', () => {
     it('should calculate the correct age based on the provided date of birth', () => {
         // Test case 1: Valid date of birth
@@ -11,4 +13,24 @@ describe('calculateAge', () => {
         expect(result2).toBe(false);
 
     })
+});
+
+describe('calculateTax', () => {
+    it('should calculate the correct tax based on the provided salary', () => {
+        // Test case 1: Salary within the first bracket
+        const result1 = calculateTax(10000);
+        expect(result1).toBe(0);
+
+        // Test case 2: Salary within the second bracket
+        const result2 = calculateTax(15000);
+        expect(result2).toBe(125);
+
+        // Test case 3: Salary within the third bracket
+        const result3 = calculateTax(19000);
+        expect(result3).toBe(325);
+
+        // Test case 4: Salary above all brackets
+        const result4 = calculateTax(30000);
+        expect(result4).toBe(875);
+    });
 });
